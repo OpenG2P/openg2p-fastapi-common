53d6d2dc88c5b260074e69c9c900ad0a6c1b7d02
PSNAppZ <thepsnarayanan@gmail.com>
2025-10-03 11:15:29 +0530

    pre-commit fixes

--- openg2p-fastapi-auth/src/openg2p_fastapi_auth/beneficiary_token.py
+++ openg2p-fastapi-auth/src/openg2p_fastapi_auth/beneficiary_token.py
@@ -1,26 +1,27 @@
 from fastapi import Request
-
 from openg2p_fastapi_common.errors.http_exceptions import (
     ForbiddenError,
 )
-from .models.credentials import AuthCredentials
+
 from .dependencies import JwtBearerAuth, UserTypeEnum
+from .models.credentials import AuthCredentials
 
 
 class BeneficiaryToken(JwtBearerAuth):
     def __init__(self):
         super().__init__()
-        self.required_user_type = UserTypeEnum.BENEFICIARY.value #TODO: Change to ENUM
+        self.required_user_type = UserTypeEnum.BENEFICIARY.value  # TODO: Change to ENUM
 
     async def __call__(self, request: Request):
-
         auth_credentials: AuthCredentials = await super().__call__(request)
         if not auth_credentials:
             return None
         claims = auth_credentials.model_dump()
 
         iss = claims.get("iss", "") or ""
-        is_keycloak = bool((claims.get("realm_access") or claims.get("resource_access")) or ("/realms/" in iss))
+        is_keycloak = bool(
+            (claims.get("realm_access") or claims.get("resource_access")) or ("/realms/" in iss)
+        )
 
         if is_keycloak:
             # Enforce userType + bene role on Keycloak
--- openg2p-fastapi-auth/src/openg2p_fastapi_auth/staff_token.py
+++ openg2p-fastapi-auth/src/openg2p_fastapi_auth/staff_token.py
@@ -1,16 +1,14 @@
 from fastapi import Request
+from openg2p_fastapi_common.errors.http_exceptions import ForbiddenError
 
-from openg2p_fastapi_common.errors.http_exceptions import (
-    ForbiddenError
-)
-from .models.credentials import AuthCredentials
 from .dependencies import JwtBearerAuth, UserTypeEnum
+from .models.credentials import AuthCredentials
 
 
 class StaffToken(JwtBearerAuth):
     def __init__(self):
         super().__init__()
-        self.required_user_type = UserTypeEnum.STAFF.value #TODO: Change to ENUM
+        self.required_user_type = UserTypeEnum.STAFF.value  # TODO: Change to ENUM
 
     async def __call__(self, request: Request):
         # First, perform the standard JWT verification and route gating
@@ -20,14 +18,18 @@ class StaffToken(JwtBearerAuth):
         claims = auth_credentials.model_dump()
 
         iss = claims.get("iss", "") or ""
-        is_keycloak = bool((claims.get("realm_access") or claims.get("resource_access")) or ("/realms/" in iss))
+        is_keycloak = bool(
+            (claims.get("realm_access") or claims.get("resource_access")) or ("/realms/" in iss)
+        )
 
         if is_keycloak:
             # Enforce userType + bene role on Keycloak
             if self.required_user_type and claims.get("userType") != self.required_user_type:
                 raise ForbiddenError(message="Forbidden. Invalid userType.")
-            realm_roles = set(((claims.get("realm_access") or {}).get("roles") or []))
-            client_roles = set((((claims.get("resource_access") or {}).get("staff-portal") or {}).get("roles") or []))
+            realm_roles = set((claims.get("realm_access") or {}).get("roles") or [])
+            client_roles = set(
+                ((claims.get("resource_access") or {}).get("staff-portal") or {}).get("roles") or []
+            )
             effective_roles = realm_roles | client_roles
             if UserTypeEnum.STAFF.value not in effective_roles:
                 raise ForbiddenError(message="Forbidden. Missing required role(s).")
